c236152cfe05679b7d2ad34bdff74f7959848379
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-08-04 16:36:57 +0900

    fix: Doc 설명 수정

--- app/src/main/java/com/devhjs/plantdex/domain/model/DexEntry.kt
+++ app/src/main/java/com/devhjs/plantdex/domain/model/DexEntry.kt
@@ -3,7 +3,7 @@ package com.devhjs.plantdex.domain.model
 /**
  * 도감에 등록된 식물.
  *
- * [id] - 발견 순서대로 1 부터 부여된다.
+ * [dexNumber] - 발견 순서대로 1 부터 부여된다.
  * [photoUri] - 촬영 사진의 위치. 아직 카메라가 붙지 않아 현재는 항상 null 이고, 화면은 플레이스홀더로 대체한다.
  */
 data class DexEntry(
