6f08cea38e20f93a62afde5635fd16d0637f26e2
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-08-04 16:36:43 +0900

    feat: di module 이름 변경

---
+++ app/src/main/java/com/devhjs/plantdex/core/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.devhjs.plantdex.core.di
+
+import com.devhjs.plantdex.data.repository.InMemoryDexRepository
+import com.devhjs.plantdex.domain.repository.DexRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Room 구현이 생기면 여기 바인딩만 갈아끼우면 된다. -> Flavor 이용 예정
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindDexRepository(repository: InMemoryDexRepository): DexRepository
+}
