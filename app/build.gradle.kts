bfb6d5dc7cae43dac1d05e55b91dc48f04ed8d24
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-08-04 16:11:45 +0900

    feat: navigation3 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    // Navigation 3
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+
     // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
